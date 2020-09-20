d5898b142a048392cefea8c2bf60a2c22c3a669d
Emerson Borges <borges.emerson.dba@gmail.com>
2020-09-20 20:26:14 -0300

    Criando navegação com rotas

--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 import './styles.css';
 
 const Header = () => (
     <header className="main-header">
-            <Logo />
+        <Logo />
+        <Link to="/">
             <div className="logo-text">
                 <span className="logo-text-1">BigGame</span>
                 <span className="logo-text-2"> Survey</span>
             </div>
-        </header>
+        </Link>
+    </header>
 )
 
 export default Header;
